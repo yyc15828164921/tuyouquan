65b7b767f2290acb918f22059fe086b4dc191fe2
后端-杨雨川 <15828164921@163.com>
2018-11-20 16:56:57 +0800

    admin/index

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yueling
+ * Date: 2018/11/20
+ * Time: 16:47
+ */
+namespace app\admin\controller;
+
+use think\Controller;
+
+Class Index extends Controller
+{
+    public function index()
+    {
+        return view();
+    }
+}
